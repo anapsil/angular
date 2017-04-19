242e8378247ee8dca9cd6201acad97a4d7182e3e
Ana Paula da Silva <anapsil83@gmail.com>
2017-04-18 21:51:57 -0300

    Add files

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { DetalheCursoComponent } from './curso/detalhe-curso/detalhe-curso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component,
+    CursosComponent,
+    DetalheCursoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
